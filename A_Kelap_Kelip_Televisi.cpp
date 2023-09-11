dd954e815a9a60bac54872b8fb770f0b5aeb7d9c
Ainun Najib <ainunnajib@gmail.com>
2023-09-12 07:36:09 +0800

    Subtask 1: given the testcase

---
+++ A_Kelap_Kelip_Televisi.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Subtask 1
+    cout << 1 << endl;
+    cout << 0 << endl;
+    cout << 0 << endl;
+    cout << 0 << endl;
+    cout << 1 << endl;
+}
