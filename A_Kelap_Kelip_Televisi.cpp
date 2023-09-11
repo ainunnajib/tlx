6e87b537c26c554839ec0db3db6f60c6d59a1d08
Ainun Najib <ainunnajib@gmail.com>
2023-09-12 07:40:05 +0800

    Subtask 2: flip everything --> p%2

--- A_Kelap_Kelip_Televisi.cpp
+++ A_Kelap_Kelip_Televisi.cpp
@@ -2,10 +2,16 @@
 using namespace std;
 
 int main(){
-    // Subtask 1
-    cout << 1 << endl;
-    cout << 0 << endl;
-    cout << 0 << endl;
-    cout << 0 << endl;
-    cout << 1 << endl;
+    int n, p; cin >> n >> p;
+    int t[p], w[p];
+    for(int i=0;i<p;i++) cin >> t[i] >> w[i];
+
+    int q; cin >> q;
+    while(q--){
+        // Subtask 2: flip everything every time
+        // odd times --> all on
+        // even times --> all off
+
+        cout << p%2 << endl;
+    }
 }
