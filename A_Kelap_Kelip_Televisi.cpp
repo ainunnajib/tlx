52310c7134935c0a224c389e5f17d4245dd0d4c8
Ainun Najib <ainunnajib@gmail.com>
2023-09-12 07:50:40 +0800

    Subtask 3: n, p, q <= 200, brute force O(n^2 * p)

--- A_Kelap_Kelip_Televisi.cpp
+++ A_Kelap_Kelip_Televisi.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main(){
@@ -6,12 +7,33 @@ int main(){
     int t[p], w[p];
     for(int i=0;i<p;i++) cin >> t[i] >> w[i];
 
-    int q; cin >> q;
-    while(q--){
-        // Subtask 2: flip everything every time
-        // odd times --> all on
-        // even times --> all off
+    //Subtask 3: n, p, q <= 200
+    // we can do brute force O(n^2 * p)
+    // maximum = 200^2 * 200 = 8,000,000
 
-        cout << p%2 << endl;
+    int tv[n][n];
+    memset(tv,0,sizeof(tv)); //tv mati semua di awal
+
+    for(int i=0;i<p;i++){
+        if(t[i] == 1){
+            for(int x=0;x<w[i];x++){
+                for(int y=0;y<w[i];y++){
+                    tv[x][y] = 1 - tv[x][y];
+                }
+            }
+        } else {
+            for(int x=w[i]-1;x<n;x++){
+                for(int y=w[i]-1;y<n;y++){
+                    tv[x][y] = 1 - tv[x][y];
+                }
+            }
+        }
+    }
+
+    int q; cin >> q;
+    int x, y;
+    while(q--){
+        cin >> x >> y;
+        cout << tv[x-1][y-1] << endl;
     }
 }
