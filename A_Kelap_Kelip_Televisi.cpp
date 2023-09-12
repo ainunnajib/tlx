813e4629ba3c885957b4465e24c50b74936b0e69
Ainun Najib <ainunnajib@gmail.com>
2023-09-12 08:44:49 +0800

    Subtask 7: n <= 200K, T=1, radial, sort from back

--- A_Kelap_Kelip_Televisi.cpp
+++ A_Kelap_Kelip_Televisi.cpp
@@ -7,21 +7,25 @@ int main(){
     int t[p], w[p];
     for(int i=0;i<p;i++) cin >> t[i] >> w[i];
 
-    //Subtask 6: ada 2 perubahan
-    // di titik yang sama tapi satu t = 1, satu t = 2
+    //Subtask 7: n <= 200,000, T always 1
+    // selalu pojok kiri atas --> radial ke kanan bawah
+    // hati-hati titik yang sama bisa diupdate berkali-kali
+
+    int tv[n+1];
+    sort(w, w+p);
+    int j = p-1, nyala = 0;
+    for(int i=n;i>0;i--){
+        while(i == w[j]){
+            nyala = 1 - nyala;
+            j--;
+        }
+        tv[i] = nyala;
+    }
 
     int q; cin >> q;
     int x, y;
     while(q--){
         cin >> x >> y;
-        
-        if(x == w[0] && y == w[0])
-            cout << 0 << endl;
-        else if(x <= w[0] && y <= w[0])
-            cout << 1 << endl;
-        else if(x >= w[0] && y >= w[0])
-            cout << 1 << endl;
-        else
-            cout << 0 << endl;
+        cout << tv[max(x, y)] << endl;
     }
 }
