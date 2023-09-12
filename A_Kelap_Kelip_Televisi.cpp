19d02411eca92b662393200952afd1dd57c0e780
Ainun Najib <ainunnajib@gmail.com>
2023-09-12 08:13:58 +0800

    Subtask 4: n,p,q <= 2000, O(np) by diagonal 1 & 2

--- A_Kelap_Kelip_Televisi.cpp
+++ A_Kelap_Kelip_Televisi.cpp
@@ -7,25 +7,27 @@ int main(){
     int t[p], w[p];
     for(int i=0;i<p;i++) cin >> t[i] >> w[i];
 
-    //Subtask 3: n, p, q <= 200
-    // we can do brute force O(n^2 * p)
-    // maximum = 200^2 * 200 = 8,000,000
+    //Subtask 4: n, p, q <= 2000
+    // observe that we can separate T = 1 vs T = 2
+    // O(n*p) solution
+    // treat T = 1 & T = 2 separately
+    // take the diagonal for each
+    // for each Q, 
+    //   check max(x, y) in diagonal 1
+    //   check min(x, y) diagonal 2
 
-    int tv[n][n];
-    memset(tv,0,sizeof(tv)); //tv mati semua di awal
+    int tv1[n], tv2[n];
+    memset(tv1,0,sizeof(tv1)); //tv mati semua di awal
+    memset(tv2,0,sizeof(tv2)); //tv mati semua di awal
 
     for(int i=0;i<p;i++){
         if(t[i] == 1){
             for(int x=0;x<w[i];x++){
-                for(int y=0;y<w[i];y++){
-                    tv[x][y] = 1 - tv[x][y];
-                }
+                tv1[x] = 1 - tv1[x];
             }
         } else {
             for(int x=w[i]-1;x<n;x++){
-                for(int y=w[i]-1;y<n;y++){
-                    tv[x][y] = 1 - tv[x][y];
-                }
+                tv2[x] = 1 - tv2[x];
             }
         }
     }
@@ -34,6 +36,7 @@ int main(){
     int x, y;
     while(q--){
         cin >> x >> y;
-        cout << tv[x-1][y-1] << endl;
+        x--; y--;
+        cout << (tv1[max(x, y)] + tv2[min(x, y)])%2 << endl;
     }
 }
